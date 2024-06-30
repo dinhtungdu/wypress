885589427c01005a6d57ca033c0ab4efde34f07c
Dharmesh Patel <dspatel44@gmail.com>
2024-06-30 11:49:51 +0530

    Fix initilize script.

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-npm run env run tests-wordpress "chmod -c ugo+w /var/www/html"
-npm run env run tests-cli "wp rewrite structure '/%postname%/' --hard"
+wp-env run tests-wordpress chmod -c ugo+w /var/www/html
+wp-env run tests-cli wp rewrite structure '/%postname%/' --hard
 
-npm run env run tests-cli "wp user create user1 user1@example.test --role=author --user_pass=password1"
-npm run env run tests-cli "wp user create user2 user2@example.test --role=author --user_pass=password2"
+wp-env run tests-cli wp user create user1 user1@example.test --role=author --user_pass=password1
+wp-env run tests-cli wp user create user2 user2@example.test --role=author --user_pass=password2
